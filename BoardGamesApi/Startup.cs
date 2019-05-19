c204a9f9895164ad1f1172e987172ff35069624e
Miroslav Popovic <miroslav.popovic@gmail.com>
2019-05-19 11:40:54 +0200

    Add project files.

---
+++ BoardGamesApi/Startup.cs
@@ -0,0 +1,50 @@
+using BoardGamesApi.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BoardGamesApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGamesRepository, GamesRepository>();
+
+            services.AddControllers()
+                .AddNewtonsoftJson();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Production-Ready APIs sample
+
+A sample code for a presentation "Building production-ready APIs with ASP.NET".
+
+This repository contains the sample implementation that's using ASP.NET Core 3.0 Preview 5 and requires Visual Studio 2019 Preview or later or .NET Core SDK 3.0.100 Preview 5 to build. 
+
+For an older version of the sample that's using ASP.NET Core 2.2 and for older presentations visit [this GitHub repository](https://github.com/miroslavpopovic/production-ready-apis-sample-2.2).
+
+To follow up the presentation, switch through the various numbered git branches.
+
+## Download (TODO)
+
+- [Postman collection](board-games-api-postman-collection.json) for testing the API
